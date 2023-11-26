9a293861433d358a3121c3f089e4370836fa0fd5
Eli Bautista <it.elibautista@gmail.com>
2023-11-26 22:57:19 +0800

    updated all files / final commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,32 +4,27 @@ const navbarToggler = document.querySelector(".navbar-toggler");
 const icon = document.getElementById("navbar-toggler-label");
 const screenWidth = window.innerWidth;
 let groupResult = 1;
-let moreResultsAvailable = true; // Flag to track if more results are available
+let moreResultsAvailable = true;
 
-// Event listener for when the page is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
     // Adjust tagline margin based on screen size
-    tagline.style.setProperty("margin-top", screenWidth < 480 ? "6vh" : "13vh", "important");
+    tagline.style.setProperty("margin-top", screenWidth < 480 ? "4vh" : "13vh", "important");
 
-    // Event listener for dropdown selection change
     const categoriesDropDown = document.getElementById('categoriesDropDown');
     if (categoriesDropDown) {
-        categoriesDropDown.addEventListener('change', function () {
-            // Call the function to perform category-based search
-            performCategorySearch();
-        });
+        // Event listener for dropdown selection change
+        categoriesDropDown.addEventListener('change', performCategorySearch);
     }
 
-    // Event listener for navbar toggler click
     navbarToggler.addEventListener("click", function () {
         // Toggle between navbar icons
         icon.classList.toggle("fa-bars");
         icon.classList.toggle("fa-xmark");
     });
 
-    // Event listener for form submission
     const searchForm = document.querySelector('form');
     if (searchForm) {
+        // Event listener for form submission
         searchForm.addEventListener('submit', function (event) {
             // Prevent the default form submission
             event.preventDefault();
@@ -57,14 +52,14 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 });
 
-// Function to load more results
 function loadMoreResults() {
+    // Function to load more results
     groupResult++; // Increment the page number
     performSearch();
 }
 
-// Function to perform search based on input and page
 function performSearch() {
+    // Function to perform search based on input and page
     const searchInput = document.getElementById('searchInput');
     if (searchInput) {
         const searchTerm = searchInput.value;
@@ -86,11 +81,11 @@ function performSearch() {
     }
 }
 
-// Function to perform category-based search
 function performCategorySearch() {
+    // Function to perform category-based search
     // Display tagline with adjusted margin
     tagline.style.display = 'block';
-    tagline.style.setProperty("margin-top", screenWidth < 480 ? "6vh" : "13vh", "important");
+    tagline.style.setProperty("margin-top", screenWidth < 480 ? "4vh" : "13vh", "important");
 
     // Fetch data from Open Food Facts API based on selected category
     const category = document.getElementById('categoriesDropDown');
@@ -117,8 +112,8 @@ function performCategorySearch() {
     }
 }
 
-// Function to update product cards on the page
 function updateProductCard(products) {
+    // Function to update product cards on the page
     // Get the products container element
     const productsContainer = document.getElementById('products-container');
 
@@ -142,14 +137,14 @@ function updateProductCard(products) {
     }
 }
 
-// Function to create a product card element
 function createProductCard(product) {
+    // Function to create a product card element
     const card = document.createElement('div');
     card.className = 'col-sm g-5';
     card.innerHTML = `
         <div class="card mx-auto shadow" style="width: 18rem;">
-            <div class="card-header text-center text-dark fw-bold">${capitalize(removeEnPrefix(product.product_name) || removeEnPrefix(product.product_name) || 'No product name to show.')}</div>
-            <a href="${product.image_url || 'https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg'}" target="_blank">
+            <div class="card-header text-center text-dark fw-bold">${capitalize(removeEnPrefix(product.product_name) || 'No product name to show.')}</div>
+            <a href="#" data-bs-toggle="modal" data-bs-target="#imageModal" data-src="${product.image_url || 'https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg'}" alt="${capitalize(removeEnPrefix(product.product_name) || 'No alt value')}">
                 <img class="card-img-top img-fluid" src="${product.image_url || 'https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg'}" alt="${capitalize(removeEnPrefix(product.product_name) || 'No alt value')}">
             </a>
             <div class="card-body">
@@ -168,54 +163,53 @@ function createProductCard(product) {
     return card;
 }
 
-// Function to show or hide the "Show More" button based on results
 function toggleShowMoreButton() {
+    // Function to show or hide the "Show More" button based on results
     const showMoreButton = document.getElementById('showMoreButton');
     if (showMoreButton) {
         showMoreButton.style.display = moreResultsAvailable ? 'block' : 'none';
     }
 }
 
-// Function to show the result modal and adjust tagline margin
 function showResultModal() {
+    // Function to show the result modal and adjust tagline margin
     var resultModal = new bootstrap.Modal(document.getElementById('resultModal'));
     resultModal.show();
     tagline.style.display = 'block';
-    tagline.style.setProperty("margin-top", screenWidth < 480 ? "6vh" : "13vh", "important");
+    tagline.style.setProperty("margin-top", screenWidth < 480 ? "4vh" : "13vh", "important");
 }
 
-// Function to hide the tagline
 function hideTagline() {
+    // Function to hide the tagline
     tagline.style.display = 'none';
     tagline.style.removeProperty('margin-top');
 }
 
-// Function to capitalize the first letter of a string
 function capitalize(str) {
+    // Function to capitalize the first letter of a string
     return typeof str === 'string' ? str.charAt(0).toUpperCase() + str.slice(1) : str;
 }
 
-// Function to remove "En:" prefix
 function removeEnPrefix(str) {
+    // Function to remove "En:" prefix
     return typeof str === 'string' ? str.replace(/^En:/i, '').trim() : str;
 }
 
-// Function to handle errors
 function handleError(error) {
-    // Log and display an error message
+    // Function to handle errors
     console.error(`Error: ${error}`);
     var maintenanceModal = new bootstrap.Modal(document.getElementById('maintenanceModal'));
     maintenanceModal.show();
     console.error('Failed to fetch data from Open Food Facts API. Please try again later.', error);
 }
 
-// Function to scroll to the top of the page
 function backToTop() {
+    // Function to scroll to the top of the page
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-// Show/hide the scroll to top button based on scroll position
 window.onscroll = function () {
+    // Show/hide the scroll to top button based on scroll position
     var backToTopButton = document.getElementById('backToTopButton');
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         backToTopButton.style.display = 'block';
@@ -223,3 +217,18 @@ window.onscroll = function () {
         backToTopButton.style.display = 'none';
     }
 };
+
+document.getElementById('imageModal').addEventListener('show.bs.modal', function (event) {
+    // Event listener for modal show event
+    const imageSrc = event.relatedTarget.getAttribute('data-src');
+    // Set the src attribute of the modal image
+    const modalImage = document.getElementById('modalImage');
+    modalImage.src = imageSrc;
+    modalImage.alt = event.relatedTarget.getAttribute('alt');
+});
+
+document.getElementById('imageModal').addEventListener('hide.bs.modal', function () {
+    // Event listener for modal hide event
+    // Clear the src attribute when the modal is hidden
+    document.getElementById('modalImage').src = '';
+});
